1fe4334c3ad74515dc1c03bc50e7d96eb84fb693
Shawn Martin-Truesdell <shawntruesdell@gmail.com>
2021-02-18 11:29:07 -0800

    Add: Updated euler1-9 problems with fixes from linter

--- problems/euler-1.ts
+++ problems/euler-1.ts
@@ -1,12 +1,12 @@
 /**
- * If we list all the natural numbers below 10 that are 
- * multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of 
+ * If we list all the natural numbers below 10 that are
+ * multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of
  * these multiples is 23.
- * 
+ *
  * Find the sum of all the multiples of 3 or 5 below 1000.
  */
-export function find_multiples_of_three_or_five() {
+export function find_multiples_of_three_or_five(max_value = 1000): number {
   // TODO
-  
+
   throw new Error('Not Yet Implemented');
 }
--- problems/euler-9.ts
+++ problems/euler-9.ts
@@ -1,18 +1,18 @@
 /**
- * A Pythagorean triplet is a set of three natural 
+ * A Pythagorean triplet is a set of three natural
  * numbers, a < b < c, for which,
- * 
+ *
  * a2 + b2 = c2
- * 
+ *
  * For example, 32 + 42 = 9 + 16 = 25 = 52.
- * 
+ *
  * There exists exactly one Pythagorean triplet for
  * which a + b + c = 1000.
- * 
+ *
  * Find the product abc.
  */
-export function product_of_pythagorean_triplet() {
+export function product_of_pythagorean_triplet(target_num = 1000): number {
   // TODO
 
-  throw new Error('Not Yet Implemented')
+  throw new Error('Not Yet Implemented');
 }
