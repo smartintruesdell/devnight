1dc0b0d79c63f497a4a47ee28b3a8f90c9ec0fe6
Shawn Martin-Truesdell <shawntruesdell@gmail.com>
2021-02-18 11:59:58 -0800

    Add: Added Euler-9 solution

--- problems/euler-9.ts
+++ problems/euler-9.ts
@@ -12,7 +12,20 @@
  * Find the product abc.
  */
 export function product_of_pythagorean_triplet(target_num = 1000): number {
-  // TODO
-
-  throw new Error('Not Yet Implemented');
+  return find_first_pythagorean_triplet(target_num).reduce(prod, 1);
 }
+
+const prod = (a: number, b: number): number => a * b;
+
+const find_first_pythagorean_triplet = (target_num: number) => {
+  let c;
+  for (let a = 1; a < Math.floor(target_num / 3); a += 1) {
+    for (let b = a + 1; b < Math.floor(target_num / 2); b += 1) {
+      c = target_num - a - b;
+      if (c > b && a * a + b * b === c * c) {
+        return [a, b, c];
+      }
+    }
+  }
+  throw new Error('No matching pythagorean triplet was found.');
+};
