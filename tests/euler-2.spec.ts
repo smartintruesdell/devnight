ca9fd1eb630c820eb4c258f82231d2d6afdaed38
Shawn Martin-Truesdell <shawntruesdell@gmail.com>
2021-02-18 10:12:15 -0800

    Add: Add Euler-2 Solution

--- tests/euler-2.spec.ts
+++ tests/euler-2.spec.ts
@@ -2,11 +2,11 @@ import { sum_even_fibonnaci_numbers } from '../problems/euler-2';
 
 jest.setTimeout(60000); // Sixty-Second Rule
 
-describe.skip('Euler Problem 2', () => {
-  test('sum_even_fibonnaci_numbers() -> number', () => {
-    const result = sum_even_fibonnaci_numbers()
-
-    expect(typeof result).toBe('number');
-    expect(result).toEqual(4613732);
-  });
+describe('Euler Problem 2', () => {
+  test.each([
+    [10, 10],
+    [4_000_000, 4613732]
+])('sum_even_fibonnaci_numbers(%d) -> %d', (input:number, expected:number) => {
+    expect(sum_even_fibonnaci_numbers(input)).toEqual(expected);
+});
 });
