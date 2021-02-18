29bcb71fc37c672dc43d539796580df46a929e98
Shawn Martin-Truesdell <shawntruesdell@gmail.com>
2021-02-18 10:11:58 -0800

    Add: Add Euler-1 Solution

--- problems/euler-1.ts
+++ problems/euler-1.ts
@@ -5,8 +5,48 @@
  *
  * Find the sum of all the multiples of 3 or 5 below 1000.
  */
-export function find_multiples_of_three_or_five(max_value = 1000): number {
-  // TODO
 
-  throw new Error('Not Yet Implemented');
+// Definitions ////////////////////////////////////////////////////////////////
+
+/** A number is a multiple of a divisor if its modulus of the divisor is 0. */
+const is_multiple_of = (divisor: number) => (n: number): boolean =>
+  n % divisor === 0;
+
+/** Checks a set of divisors against a number for is_multiple_of */
+const is_any_multiple_of = (divisors: number[]) => (n: number): boolean =>
+  divisors.reduce(
+    (acc: boolean, divisor: number) => acc || is_multiple_of(divisor)(n),
+    false
+  );
+
+/**
+ * Finds the SUM of all matching multiples of an array of divisors
+ * below a given threshold.
+ *
+ * @param { number[] } divisors
+ *  Values less than the threshold that are even multiples of at
+ *  least one member of this array are added to the sum.
+ * @param { number } max_value The upward bound (exclusive) to check
+ *
+ * @returns { number } Returns the sum of all matching values.
+ */
+export function find_even_multiples(
+  divisors: number[] = [3, 5],
+  max_value: number = 1000
+): number {
+  let sum = 0;
+
+  // For each number in our set from 0 to max_value...
+  for (let i = 0; i < max_value; i += 1) {
+    // For each divisor that could match...
+    if (is_any_multiple_of(divisors)(i)) {
+      // Add matching values to the sum
+      sum += i;
+    }
+  }
+
+  // Return the sum
+  return sum;
 }
+
+export const find_multiples_of_three_or_five = find_even_multiples;
