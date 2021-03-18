e0367f460eda967b64e70b23f4e73a96458214ff
Shawn Martin-Truesdell <shawn@martin-truesdell>
2021-03-17 19:55:19 -0700

    Co-authored-by: JediBrown <JediBrown@users.noreply.github.com>

--- tests/mirror-properties.spec.ts
+++ tests/mirror-properties.spec.ts
@@ -1,37 +1,20 @@
-/** Tests for problems/mirror-properties.ts */
-import { fillWithReversedKeys } from '../problems/mirror-properties';
+import {
+  fillWithReversedKeys,
+  InputObjectShape,
+} from '../problems/mirror-properties';
 
-describe('fillWithReversedKeys', () => {
-  // Add tests for component functions, if necessary
+describe.only('fillWithReversedKeys', () => {
+  test('reverses keys', () => {
+    const inputObject: InputObjectShape = {
+      first: undefined,
+      second: undefined,
+      third: undefined,
+    };
 
-  // Solution tests are prebaked, you can leave them alone.
-  describe('solution', () => {
-    test.each([
-      [{ first: undefined }, { first: 'tsrif' }],
-      [
-        { first: undefined, second: undefined },
-        { first: 'tsrif', second: 'dnoces' },
-      ],
-      [
-        { first: undefined, second: undefined, third: undefined },
-        { first: 'tsrif', second: 'dnoces', third: 'driht' },
-      ],
-    ])('fillWithReversedKeys(%p) -> %p', (inputObject, expected) =>
-      expect(fillWithReversedKeys(inputObject)).toStrictEqual(expected)
-      );
-
-    test('fillWithReversedKeys does NOT mutate the original object', () => {
-      // given
-      const inputObject = {
-        trouper: undefined
-      };
-
-      // when
-      const result = fillWithReversedKeys(inputObject);
-
-      // then
-      expect(result).toStrictEqual({ trouper: 'repuort' });
-      expect(inputObject).not.toStrictEqual(result);
+    expect(fillWithReversedKeys(inputObject)).toStrictEqual({
+      first: 'tsrif',
+      second: 'dnoces',
+      third: 'driht',
     });
   });
 });
